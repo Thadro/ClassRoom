88c078e4c381e663e5eeb89e1d1d032023561f07
Julien F <jfustier42@gmail.com>
2020-04-14 14:36:16 +0200

    Ajout du formulaire de création de la formateur (page administrateur.html) et de la connexion a l'espace formateur (page index.html) + début du js pour l'affichage des classes en fonction du profil du formateur (page formateur.js et formateur.html)

--- assets/js/formateur.js
+++ assets/js/formateur.js
@@ -1,3 +1,23 @@
+//Initialisation de l'application
+// Configuration de firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyCvIsah7MPvXALU1hiilY6pjzm7UNZi9zc",
+    authDomain: "project-classroom-c1da1.firebaseapp.com",
+    databaseURL: "https://project-classroom-c1da1.firebaseio.com",
+    projectId: "project-classroom-c1da1",
+    storageBucket: "project-classroom-c1da1.appspot.com",
+    messagingSenderId: "613309391988",
+    appId: "1:613309391988:web:87116cee59aff788fe5d21"
+  };
+// Initialisation Firebase
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+//PARTIE CLASSE
+//Affichage de la classe du formateur
+
+
 //Affichage tableau des élèves
 $('#classroom-section-title').click(function(){
     $('#classroom-section-title').css("height", "50px");
